6f0baa6140c4a018bf1913c6f082e71586d591b0
Bholi <najmulbro123@gmail.com>
2024-04-25 16:59:42 +0545

    Custom Views Logic

--- base/urls.py
+++ base/urls.py
@@ -5,8 +5,11 @@ urlpatterns = [
    path('resource-type/<int:pk>/',ResourceTypeView.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
    path('department/',DepartmentView.as_view({'get':'list','post':'create'})),
    path('department/<int:pk>/',DepartmentView.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
-   path('resource/',ResourceView.as_view({'get':'list','post':'create'})),
-   path('resource/<int:pk>/',ResourceView.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
+   # path('resource/',ResourceView.as_view({'get':'list','post':'create'})),
+   # path('resource/<int:pk>/',ResourceView.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
+
+   path('resource/',ResourceView.as_view()),
+
    path('vendor/',VendorView.as_view({'get':'list','post':'create'})),
    path('vendor/<int:pk>/',VendorView.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
    path('purchase/',PurchaseView.as_view({'get':'list','post':'create'})),
